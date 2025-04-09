7ef0d955578e253f2b7b854826722228ab1c959b
Arya Ranjan <aarya.ranjan.bgs.2004@gmail.com>
2025-04-09 21:22:44 +0530

    Ionix.py

--- Ionix.py
+++ Ionix.py
@@ -130,7 +130,7 @@ def load_name() -> str:
         with open("assistant_name.txt", "r") as file:
             return file.read().strip()
     except FileNotFoundError:
-        return "Jarvis"  # Default name
+        return "Ionix"  # Default name
 
 
 def search_wikipedia(query):
